3518432f971227c26757b79d3481c248044e0b49
Zach Gulde <zachgulde@gmail.com>
2020-09-28 14:53:38 -0500

    Add scaling review notes

---
+++ wrangle.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+from env import host, user, password
+
+# function to get the url
+
+
+def get_db_url(db_name):
+    return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
+
+
+def get_data_from_sql():
+    query = """
+    SELECT customer_id, monthly_charges, tenure, total_charges
+    FROM customers
+    WHERE contract_type_id = 3;
+    """
+    df = pd.read_sql(query, get_db_url("telco_churn"))
+    return df
+
+
+def wrangle_telco():
+    """
+    Queries the telco_churn database
+    Returns a clean df with four columns:
+    customer_id(object), monthly_charges(float), tenure(int), total_charges(float)
+    """
+    df = get_data_from_sql()
+    df.tenure.replace(0, 1, inplace=True)
+    df.total_charges = df.total_charges.replace(" ", np.nan)
+    df.total_charges = df.total_charges.fillna(df.monthly_charges)
+    df.total_charges = df.total_charges.astype(float)
+    return df
+
+
+def wrangle_grades():
+    grades = pd.read_csv("student_grades.csv")
+    grades.drop(columns="student_id", inplace=True)
+    grades.replace(r"^\s*$", np.nan, regex=True, inplace=True)
+    df = grades.dropna().astype("int")
+    return df
